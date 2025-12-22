5a4a7e1e2808413db0971b747921429f4023e2f6
Ihor <vnykigor@gmail.com>
2025-12-22 22:38:44 +0200

    fix: prisma version

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://wallet-tracker-frontend.vercel.app',
+    ],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
